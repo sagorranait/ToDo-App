ce37e89849c55fa25ea66b29cdc50d213550bd43
SAGOR RANA <60606778+sagorranait@users.noreply.github.com>
2022-02-03 22:42:11 +0600

    list

--- js/functionality.js
+++ js/functionality.js
@@ -1,3 +1,9 @@
 function showEmoji(emojis, domElement){
-  console.log(domElement);
+  let lists = document.createElement('li');
+  let attr = document.createAttribute("class");
+  emojis.map((emoji, index) => {
+    attr.value = emoji.key;
+    lists.setAttributeNode(attr);
+    console.log(lists)
+  });
 }
