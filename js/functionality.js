372ea5b63efc1995d16f269984268891bdd366e5
SAGOR RANA <60606778+sagorranait@users.noreply.github.com>
2022-02-11 09:45:35 +0600

    update

--- js/functionality.js
+++ js/functionality.js
@@ -1,7 +1,7 @@
 function showEmoji(emojis, domElement){
   let lists = document.createElement('li');
   let attr = document.createAttribute("class");
-  emojis.map((emoji, index) => {
+  emojis.map((emoji) => {
     attr.value = emoji.key;
     lists.setAttributeNode(attr);
     console.log(lists)
